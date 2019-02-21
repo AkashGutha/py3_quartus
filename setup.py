b9dcdb754757f9c4d74c171e61a704bda2a775de
Gutha <gutha.3@asc.ohio-state.edu>
2019-02-21 18:11:01 -0500

    ADD: setup

---
+++ setup.py
@@ -0,0 +1,22 @@
+import setuptools
+
+
+with open("Readme.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="py3-quartus-akash-gutha",
+    version="0.0.1",
+    author="Akash Gutha",
+    author_email="gutha.3@buckeyemail.osu.edu",
+    description="Python 3 interface for quartus tcl programming",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/AkashGutha/py3-quartus",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: Windows",
+    ],
+)
