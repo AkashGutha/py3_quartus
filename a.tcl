0b69eb82bd1bea552bd6ab1b4d1197395eb7c2c1
Gutha <gutha.3@asc.ohio-state.edu>
2019-02-21 17:50:58 -0500

    UPDATE

--- a.tcl
+++ a.tcl
@@ -18,10 +18,10 @@ foreach device_name [get_device_names -hardware_name $usbblaster_name] {
 begin_memory_edit -hardware_name "DE-SoC \[USB-1\]" -device_name "@2: 5CSE(BA4|MA4)/5CSXFC4C6 (0x02D010DD)"
 
 # Write memory content using the hex memory file
-update_content_to_memory_from_file -instance_index 0 -mem_file_path "test.hex" -mem_file_type hex
+# update_content_to_memory_from_file -instance_index 0 -mem_file_path "memfile.hex" -mem_file_type hex
   
 # Read memory content and save back to a hex memory file
-save_content_from_memory_to_file -instance_index 0 -mem_file_path "test.hex" -mem_file_type hex
+save_content_from_memory_to_file -instance_index 0 -mem_file_path "memfile.hex" -mem_file_type hex
 
 # End the editing sequence
 end_memory_edit
--- automatic_flashing.py
+++ automatic_flashing.py
@@ -15,16 +15,3 @@ print("connected devices: " +
       ", ".join([str(device) for device in connected_devices]))
 connected_device = Device(
     name=connected_devices[1], hardware=connected_hardware)
-connected_device.write_to_memory_from_file_2(
-    path="memfile.hex", instance_index=1)
-
-
-# import sys
-# from subprocess import Popen, PIPE
-
-# from time import sleep
-
-# output, err = Popen(["quartus_stp", "-t", "a.tcl"],
-#                     shell=True, stdin=PIPE, stdout=PIPE, stderr=PIPE).communicate()
-
-# print(output)
