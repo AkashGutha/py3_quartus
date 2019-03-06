1f57e311427dc1b7a6cd761b5a50a673401312cd
AkashGutha <akash.gutha@outlook.com>
2019-03-06 03:02:34 -0500

    ADD: Selection procedure

--- examples/device_hardware_selection.py
+++ examples/device_hardware_selection.py
@@ -1,3 +1,4 @@
+import os
 import click
 from py3_quartus.Hardware import Hardware
 from py3_quartus import tcl_tools
@@ -10,7 +11,9 @@ def program(hardware, device):
     '''
     run the main program to configure and program the boards
     '''
-    hw = None
+    selected_hardware = None
+    selected_device = None
+    selected_sof_file = None
 
     if(hardware == "" or hardware == None):
         hw_names = tcl_tools.get_hardware_names()
@@ -23,6 +26,7 @@ def program(hardware, device):
         for hw in hw_list:
             print(hw)
         print("\nPlease select a Hardware from the above list")
+        selected_hardware = select_one_from_list(hw_list)
     pass
 
     if(device == "" or device == None):
@@ -36,8 +40,25 @@ def program(hardware, device):
         for dv in devices_list:
             print(dv)
         print("\nPlease select a Device from the above list")
+        selected_device = select_one_from_list(devices_list)
     pass
 
 
+def select_one_from_list(options_list=[]):
+    selected_option = None
+    if len(options_list) > 0:
+        while True:
+            print("Select one options form the list:\n")
+            for i in range(len(options_list)):
+                print(str(i+1)+": " + options_list[i])
+            inp = input("Select an option: ")
+            if (inp.isnumeric() and int(inp) > 0):
+                print("Selected option: " + str(options_list[int(inp)-1]))
+                return inp
+
+    return selected_option
+
+
 if (__name__ == "__main__"):
-    program(None, None)
+    # program(None, None)
+    select_one_from_list(["asdfsdf", 'adsfsdf', "dfasdfasd"])
